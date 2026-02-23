920f4c49caa386687a72d70ec8401a947b981ca6
lucas vinicius <lucassilved@gmail.com>
2026-02-23 14:38:53 -0300

    initial commit and basic setup

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+]);
