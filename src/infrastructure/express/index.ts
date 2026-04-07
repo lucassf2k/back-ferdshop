4c2e979521129e643e0c6153987e6b79787c11a5
lucas vinicius <lucassilved@gmail.com>
2026-04-07 15:01:12 -0300

    feat: add cors

--- src/infrastructure/express/index.ts
+++ src/infrastructure/express/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 import { errorHandler } from './middlewares/error-handler';
 
 export class ExpressApplication {
   static run(): void {
-    const ferdShopApplicationExpress = express();
-
-    ferdShopApplicationExpress.use(express.json());
-    ferdShopApplicationExpress.use('/api/v1', routes);
-    ferdShopApplicationExpress.use(errorHandler);
-    ferdShopApplicationExpress.listen(3001, () => {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use('/api/v1', routes);
+    app.use(errorHandler);
+    app.listen(3001, () => {
       console.log('Server listening on port 3001');
     });
   }
