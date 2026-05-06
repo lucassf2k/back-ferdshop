bf6cef3448354d91ea25ebe4145a39755c26e0fa
lucas vinicius <lucassilved@gmail.com>
2026-05-06 14:50:26 -0300

    refactor: add swagger config in express app

--- src/infrastructure/express/index.ts
+++ src/infrastructure/express/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 import { errorHandler } from './middlewares/error-handler';
+import { swaggerConfig } from '../services/swagger';
 
 export class ExpressApplication {
   static run(): void {
     const app = express();
     app.use(cors());
     app.use(express.json());
+    swaggerConfig(app);
     app.use('/api/v1', routes);
     app.use(errorHandler);
     app.listen(3001, () => {
